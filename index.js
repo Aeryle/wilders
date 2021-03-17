2139eb5168ffd60e38c70a61c60194cdd2739a9a
Aeryle <47262222+Aeryle@users.noreply.github.com>
2021-03-17 15:32:26 +0100

    Added wilders names and colors

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.cyan('Matthias Wanner'));
+console.log(chalk.blue('Julien Abaddie'));
+console.log(chalk.green('Matthieu O\'Jeanson'));
+console.log(chalk.yellow('Mathias Ryckewaert'));
